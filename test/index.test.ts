dc7265bc9113f6e19f03e758fec9ee4ea9c9946d
Jonas Claes <jonas@jonasclaes.be>
2023-02-17 14:11:00 +0100

    Added some more unit tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,3 +3,11 @@ import {sum} from "../src";
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('adds null + 1 to equal 1', () => {
+    expect(sum(null, 1)).toBe(1);
+});
+
+test('adds 1 + null to equal 1', () => {
+    expect(sum(1, null)).toBe(1);
+});
