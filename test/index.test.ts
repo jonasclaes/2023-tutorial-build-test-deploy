5aae5d6b28136ff89da52e04e81e649c35fb8533
Jonas Claes <jonas@jonasclaes.be>
2023-02-18 20:09:05 +0100

    Edited tests to be more meaningfull

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,10 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds null + 1 to equal 1', () => {
-    expect(sum(null, 1)).toBe(1);
+test('adds 0 + 1 to equal 1', () => {
+    expect(sum(0, 1)).toBe(1);
 });
 
-test('adds 1 + null to equal 1', () => {
-    expect(sum(1, null)).toBe(1);
+test('adds -1 + 1 to equal 0', () => {
+    expect(sum(-1, 1)).toBe(0);
 });
